ae609dd0d8e950f6f3cbe076e3c7f83ec9dbed9a
arul <arul@aruls-MacBook-Air.local>
2025-12-12 15:54:59 +0530

    update

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -8,3 +8,10 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
+
+// export function createClientFile() {
+//   return createBrowserClient(
+//     process.env.NEXT_LIVE_SUPABASE_URL!,
+//     process.env.NEXT_LIVE_SUPABASE_ANON_KEY!
+//   )
+// }
