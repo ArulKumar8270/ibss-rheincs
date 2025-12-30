3b596af2d4cfc4e2a6904cbd06c2dbef988f3667
Padma <25895padma@gmail.com>
2025-12-30 16:39:28 +0530

    changes

--- app/digital-services/page.tsx
+++ app/digital-services/page.tsx
@@ -25,6 +25,112 @@ export default function AlMl() {
               we ensure every engagement delivers measurable, long-term value,
               with solutions that are scalable, agile, and future-ready.
             </p>
+            
+                        <div className="ser-btn mt-5">
+                          <Link href="/contact-us" className="animated-svg-link">
+                            Contact Us
+                            <span className="svg-container ">
+                              <span className=" right">
+                                <svg
+                                  width={24}
+                                  height={23}
+                                  viewBox="0 0 24 23"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="16.0004"
+                                    cy="4.79995"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="12.7992"
+                                    cy="1.6"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="22.4008"
+                                    cy="11.2"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="1.6"
+                                    cy="11.2"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="6.40078"
+                                    cy="11.2"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="11.1996"
+                                    cy="11.2"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="16.0004"
+                                    cy="11.2"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="19.1996"
+                                    cy="14.4"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="16.0004"
+                                    cy="17.6"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="12.7992"
+                                    cy="20.8"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                  <circle
+                                    className="dot"
+                                    opacity="0.5"
+                                    cx="19.1996"
+                                    cy="8.00002"
+                                    r="1.6"
+                                    fill="#535353"
+                                  />
+                                </svg>
+                              </span>
+                            </span>
+                          </Link>
+                        </div>
           </div>
         </div>
         <div className="col-sm-6">
--- app/solutions/digital-services-and-solutions/page.tsx
+++ app/solutions/digital-services-and-solutions/page.tsx
@@ -19,8 +19,7 @@ export default function digitalsolution() {
               Innovative Digital Services
             </h2>
             <p>
-              We deliver outcome-driven solutions that combine advanced
-              technology, proven frameworks, and scalable architectures to solve
+              We deliver outcome-driven digital transformation powered by advanced technology and proven frameworks, and scalable architectures to solve
               complex business challenges. Driven by our
               Analyse-Integrate-Transform framework, every engagement is built
               for measurable, sustained impact.
