6fb102e64b57a83bb264c8af5f0d00b0eef03208
Padma <25895padma@gmail.com>
2026-01-08 10:36:33 +0530

    changes

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+'use client';
+
 import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -412,11 +414,11 @@ export default function Header() {
                                             xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M17 20.5H7C4 20.5 2 19 2 15.5V8.5C2 5 4 3.5 7 3.5H17C20 3.5 22 5 22 8.5V15.5C22 19 20 20.5 17 20.5Z"
-                                                stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
-                                                stroke-linejoin="round" />
+                                                stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round"
+                                                strokeLinejoin="round" />
                                             <path d="M17 9L13.87 11.5C12.84 12.32 11.15 12.32 10.12 11.5L7 9" stroke="white"
-                                                stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
-                                                stroke-linejoin="round" />
+                                                strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round"
+                                                strokeLinejoin="round" />
                                         </svg>
                                     </span>
                                     <Link href="mailto:info@rheincs.com"> info@rheincs.com</Link>
