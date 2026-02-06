8d88052726d817047b3c3f7bd0df951129bf0d6a
Padma <25895padma@gmail.com>
2026-02-06 14:44:08 +0530

    changes

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -479,7 +479,7 @@ export default function Header() {
                                     </span>
                                     <Link href="mailto:info@rheincs.com"> info@rheincs.com</Link>
                                 </li>
-                                 <li>
+                                 {/* <li>
                                      <div className="custom-select top-icon-gap" style={{ position: 'relative' }} ref={dropdownRef}>
                                         <select
                                             ref={selectRef}
@@ -573,7 +573,7 @@ export default function Header() {
                                             </div>
                                         )}
                                     </div>
-                                </li> 
+                                </li>  */}
                                 {/* <li>
                                     <div id="chcp_font_size" className="input-group">
                                         <span className="input-group-btn font-increase-waber">
