56dcb5e2b69b68fee6a6c8b446349b8570de3b1d
Padma <25895padma@gmail.com>
2026-02-13 10:39:17 +0530

    changes

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1188,7 +1188,7 @@ Germany<br>
                   </div>
                   <div className="col-sm-4">
                     <div className="ser-btn text-right">
-                      <Link href="/contact" className="animated-svg-link">
+                      <Link href="/contact-us" className="animated-svg-link">
                         {t("Contact Us")}
                         <span className="svg-container ">
                           <span className=" right">
